1b59395057368f8d7fdce69ff6f51427786d11d3
ZidaanB <96749839+ZidaanB@users.noreply.github.com>
2022-01-04 12:00:16 -0800

    Update storedesign.py

--- storedesign.py
+++ storedesign.py
@@ -2,8 +2,8 @@ import turtle
 
 llcords = (-200, -200)
 currentPos = llcords
-num_rows = 6
-num_cols = 6
+num_rows = 4
+num_cols = 3
 aisle_side_len = 100
 # Draw the basic screen
 screen = turtle.getscreen()
@@ -20,4 +20,7 @@ currentPos = t.pos()
 t.goto(currentPos[0], currentPos[1] - (num_cols * aisle_side_len))
 currentPos = t.pos()
 t.goto(llcords[0],currentPos[1])
+currentPos = t.pos()
+t.penup()
+t.goto(llcords[0] + (num_rows*aisle_side_len),llcords[1] + (aisle_side_len*num_cols))
 turtle.Screen().exitonclick()
